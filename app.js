cb86900fb6ebd15e5ef088fffa7993ec01301981
NH-Rasel <mdrasel414.mr@gmail.com>
2021-09-02 03:46:11 +0600

    main-part-commited

---
+++ app.js
@@ -0,0 +1,39 @@
+const searchBook = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    searchField.value = '';
+    const url = `http://openlibrary.org/search.json?q=${searchText}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySearchResult(data.docs))
+}
+
+const displaySearchResult = docs => {
+    const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+    if (docs.length === 0) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+             <h1 style="margin-left:450px" class="text-nowrap text-center text-muted w-auto">No Search Result :(</h1>
+        `;
+        searchResult.appendChild(div);
+    }
+    else {
+        docs.forEach(book => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+                <div class="card h-100">
+                    <img src="https://covers.openlibrary.org/b/id/{cover_i}-M.jpg" class="card-img-top w-75 m-3 ps-4 pt-4" alt="...">
+                    <div class="card-body">
+                        <h4 class="card-title">${book.title}</h4>
+                        <p class="card-text"><span class="fw-bold text-secondary">Authors: </span>${book.author_name}</p>
+                        <p class="card-text text-muted">Publisher: ${book.publisher}</p>
+                        <p class="card-text text-muted">First Published: ${book.first_publish_year}</p>
+                    </div>
+                </div>
+            `;
+            searchResult.appendChild(div);
+        })
+    }
+}
