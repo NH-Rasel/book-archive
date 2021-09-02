635654b303c1f4a59435c3bb18f8f7bc88abb156
NH-Rasel <mdrasel414.mr@gmail.com>
2021-09-02 13:07:08 +0600

    final-commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// search book from input field
 const searchBook = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
@@ -8,6 +9,7 @@ const searchBook = () => {
         .then(data => displaySearchResult(data.docs))
 }
 
+// show the search result
 const displaySearchResult = docs => {
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
@@ -18,8 +20,9 @@ const displaySearchResult = docs => {
         `;
         searchResult.appendChild(div);
     }
-    else {
-        docs.forEach(book => {
+    else if (docs.length > 0) {
+        docs.length = 60;
+        docs?.forEach(book => {
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
@@ -27,9 +30,9 @@ const displaySearchResult = docs => {
                     <img src="https://covers.openlibrary.org/b/id/{cover_i}-M.jpg" class="card-img-top w-75 m-3 ps-4 pt-4" alt="...">
                     <div class="card-body">
                         <h4 class="card-title">${book.title}</h4>
-                        <p class="card-text"><span class="fw-bold text-secondary">Authors: </span>${book.author_name}</p>
-                        <p class="card-text text-muted">Publisher: ${book.publisher}</p>
-                        <p class="card-text text-muted">First Published: ${book.first_publish_year}</p>
+                        <p class="card-text"><span class="fw-bold text-secondary">Authors: </span>${book.author_name ? book.author_name : ''}</p>
+                        <p class="card-text text-muted">Publisher: ${book.publisher ? book.publisher : ''}</p>
+                        <p class="card-text text-muted">First Published: ${book.first_publish_year ? book.first_publish_year : ''}</p>
                     </div>
                 </div>
             `;
